cc590729b8ffbf801e0cd6e881b0594efa5fd421
liaoguowei <781720639@qq.com>
2017-09-11 14:23:41 +0800

    edit

--- demo/src/main/java/com/pingxun/library/guomei/MainActivity.java
+++ demo/src/main/java/com/pingxun/library/guomei/MainActivity.java
@@ -15,11 +15,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button buttonPanel=(Button)findViewById(R.id.buttonPanel);
+        Button buttonPanel = (Button) findViewById(R.id.buttonPanel);
         buttonPanel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, DemoActivity.class).putExtra("url","https://static-sit.gomemyf.com/flow-h5/#/index?fromChannel=miaobd&fromChannelId=miaobd1"));
+                startActivity(new Intent(MainActivity.this, DemoActivity.class).putExtra("url", "https://static-sit.gomemyf.com/traffic-access/#/testTool?fromChannel=miaobd&fromChannelId=miaobd-a"));
+//                startActivity(new Intent(MainActivity.this, DemoActivity.class).putExtra("url","https://static-sit.gomemyf.com/flow-h5/#/index?fromChannel=miaobd&fromChannelId=miaobd1"));
             }
         });
     }
